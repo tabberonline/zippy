01230a65817af35abaa230260e8ae17fd5e34f84
Vikraantnegi <43957569+Vikraantnegi@users.noreply.github.com>
2020-12-02 12:31:15 +0530

    ADD: GoogleAuth

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-#Zippy
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import GoogleBtn from './components/GoogleBtn';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+    <GoogleBtn />
     </div>
   );
 }
