89baeaed856346e168a31dfa58a6e4f7b07bd28b
Vikraantnegi <43957569+Vikraantnegi@users.noreply.github.com>
2020-12-05 21:23:57 +0530

    ADD: HomeScreen

---
+++ src/screens/HomeScreen/HomeScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../../styles/HelperStyles.css';
+import Header from '../../components/Header/Header';
+
+function HomeScreen() {
+  return (
+    <div className="#home-screen">
+      <Header />
+    </div>
+  );
+}
+
+export default HomeScreen;
