a1ab76a3dcc58957e03c2f6f39e808acd3bf4a05
Vikraantnegi <43957569+Vikraantnegi@users.noreply.github.com>
2020-12-12 21:38:48 +0530

    ADD : Project Section

--- src/assets/Datafiles/StudentA.js
+++ src/assets/Datafiles/StudentA.js
@@ -44,11 +44,11 @@ export default[
         achievements : [
             {
                 sr : 1,
-                namee : 'GeeksforGeeks',
+                name : 'GeeksforGeeks',
                 logo : 'https://media.geeksforgeeks.org/wp-content/cdn-uploads/20200817185016/gfg_complete_logo_2x-min.png',
                 id: 'coder1234',
                 rank : 1234,
-                contest : 'Codewars 2014 India’s FFirst Coding Competition.'
+                contest : 'Codewars 2014 India’s First Coding Competition.'
             },
         ],
         projects : [
--- src/screens/PortfolioScreen/PortfolioScreen.js
+++ src/screens/PortfolioScreen/PortfolioScreen.js
@@ -5,6 +5,7 @@ import Header from '../../components/Header/Header';
 import Footer from '../../components/Footer/Footer';
 import StudentA from '../../assets/Datafiles/StudentA';
 import {AiOutlinePlusCircle} from 'react-icons/ai';
+import project1 from '../../assets/images/StudentA/Rectangle 25.png';
 
 function PortfolioScreen() {
   return (
@@ -45,23 +46,44 @@ function PortfolioScreen() {
                 <p className="card-heading mb-20">Contests Won</p>
                 <div className="flexRow flexWrap">
                   {student.achievements.map(achievement => (
-                    <div key={achievement.sr} className="flexColumn profile-card">
+                    <div key={achievement.sr} className="flexColumn achievement-card">
                       <img className="logo" src={achievement.logo} alt="logo" />
                       <p className="profile-name pl-20 mb-10">{achievement.name}</p>
                       <div className="flexRow flexBetween"> 
-                        <p className="profile-name pl-20 mb-10"><span className="profile-heading">ID:</span> {achievement.id}</p>
-                        <p className="profile-name pl-20 mb-10"><span className="profile-heading">Rank:</span> {achievement.rank}</p>
+                        <p className="profile-name ph-20 mb-10"><span className="profile-heading">ID:</span> {achievement.id}</p>
+                        <p className="profile-name ph-20 mb-10"><span className="profile-heading">Rank:</span> {achievement.rank}</p>
                       </div>
-                      <p className="profile-name pl-20 mb-10"><span className="profile-heading">Contest Name:</span> {achievement.contest}</p>
+                      <p className="profile-name ph-20 mb-10"><span className="profile-heading">Contest Name:</span> {achievement.contest}</p>
                     </div>
                   ))}
-                  <div className="flexColumn flexCenter flexAlignCenter profile-card">
+                  <div className="flexColumn flexCenter flexAlignCenter achievement-card">
+                    <AiOutlinePlusCircle style={{fontSize: 80, color: '#C0C0C0'}} />
+                  </div>
+                </div>
+              </div>
+              <div className="coding-profile mv-20">
+                <p className="card-heading mb-20">Personal Projects</p>
+                <div className="flexRow flexWrap">
+                  {student.projects.map(project => (
+                    <div key={project.sr} className="flexColumn project-card" >
+                      <img src={project.img} alt="project" className="project-img" />
+                      {/* <div className="flexColumn flexCenter flexAlignCenter project-textbox">
+                        <p className="project-name">{project.name}</p>
+                      </div> */}
+                    </div>
+                  ))}
+                  <div className="flexColumn flexCenter flexAlignCenter project-card">
                     <AiOutlinePlusCircle style={{fontSize: 80, color: '#C0C0C0'}} />
                   </div>
                 </div>
               </div>
             </div>
           ))}
+          <div className="flexRow flexCenter ph-20 flexAlignCenter mv-40">
+            <hr style={{width: '35%', color: 'rgba(154,154,154,1)'}} />
+            <p className="end-text">Thats all folks</p>
+            <hr style={{width: '35%', color: 'rgba(154,154,154,1)'}} />
+          </div>
         </div>
       </div>
       <Footer />
