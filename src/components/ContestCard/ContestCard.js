377c1e9266c288306f804c5a5dbf3ac4291fafb9
Vikraantnegi <43957569+Vikraantnegi@users.noreply.github.com>
2020-12-28 17:41:33 +0530

    ADD: Portfolio Functionalities

--- src/components/ContestCard/ContestCard.js
+++ src/components/ContestCard/ContestCard.js
@@ -1,10 +1,65 @@
+import React, { useEffect, useState } from 'react';
 import '../../styles/HelperStyles.css';
 import './ContestCard.css';
 import {BsThreeDotsVertical} from 'react-icons/bs';
+import deleted from '../../assets/images/Bin-Icon.png';
+import edited from '../../assets/images/Edit-Icon.png';
+import hidden from '../../assets/images/Hide-Icon.png';
+import $ from 'jquery';
 
 export default function ContestCard({name, rank, id, logo, contest}){
+
+    const [ bullets, setbullets ] = useState(true);
+    const [ drawer, setdrawer ] = useState(false);
+    const [option1, setoption1] = useState(false);
+    const [option2, setoption2] = useState(false);
+    const [option3, setoption3] = useState(false);
+
+    useEffect(() => {
+        $(".option-drawer").mouseenter(() => {
+            setdrawer(true);
+            setbullets(false);
+            $(".options").mouseleave(() => {
+                console.log("Bye");
+                setdrawer(false);
+                setbullets(true);
+            })
+        })
+        $(".option-drawer").on("click", () => {
+            setdrawer(true);
+            setbullets(false);
+        })
+        $(".delete-icon").mouseenter(() => {
+            $(".delete-icon").hide();
+            setoption1(true);
+            
+            $(".delete-option").mouseleave(() => {
+                $(".delete-icon").show();
+                setoption1(false);
+            })        
+        })
+        $(".edit-icon").mouseenter(() => {
+            $(".edit-icon").hide();
+            setoption2(true);
+            
+            $(".edit-option").mouseleave(() => {
+                $(".edit-icon").show();
+                setoption2(false);
+            }) 
+        })
+        $(".hide-icon").mouseenter(() => {
+            $(".hide-icon").hide();
+            setoption3(true);
+            
+            $(".hide-option").mouseleave(() => {
+                $(".hide-icon").show();
+                setoption3(false);
+            }) 
+        })
+    })
+
     return(
-        <div className="flexColumn achievement-card">
+        <div className="card11 flexColumn achievement-card">
             <div className="flexRow flexCenter">
                 {logo === "" ? (
                     <div className="flexRow flexCenter flexAlignCenter empty-logo">
@@ -13,9 +68,41 @@ export default function ContestCard({name, rank, id, logo, contest}){
                 ) : (
                     <img className="logo" src={logo} alt="logo" />
                 )}
-                <div className="flexRow flexCenter flexAlignCenter iconcontainer" style={{top: '-15%', right:'-25%'}}>
-                    <BsThreeDotsVertical className="portfolio-icon" size="20" />
-                </div>
+                { bullets ? (
+                    <div className="flexRow option-drawer flexCenter flexAlignCenter iconcontainer" style={{top: '-15%', right:'-25%'}}>
+                        <BsThreeDotsVertical className="portfolio-icon" size="20" />                    
+                    </div>
+                    ) : null 
+                }
+                { drawer ? (
+                    <div className="flexColumn options" style={{position: 'relative', top: '-10%', right:'-25%'}}>
+                        <img src={deleted} alt="delete" className="delete-icon" style={{height:30, width: 30, marginBottom: 10}} />
+                        { option1 ? (
+                            <div className="flexRow option delete-option" style={{ marginBottom: 10 }}>
+                                <img src={deleted} alt="delete" style={{height:30, width: 30, marginRight: 10}} />
+                                <p className="options-text">Delete</p>
+                            </div>
+                            ) : null
+                        }
+                        <img src={edited} alt="edit" className="edit-icon" style={{height:30, width: 30, marginBottom: 10}} />
+                        { option2 ? (
+                            <div className="flexRow option edit-option" style={{ marginBottom: 10 }}>
+                                <img src={edited} alt="edit" style={{height:30, width: 30, marginRight: 10}} />
+                                <p className="options-text">Edit</p>
+                            </div>
+                            ) : null
+                        }
+                        <img src={hidden} alt="hide" className="hide-icon" style={{height:30, width: 30, marginBottom: 10}} />
+                        { option3 ? (
+                            <div className="flexRow option hide-option" style={{ marginBottom: 10 }}>
+                                <img src={hidden} alt="hide" style={{height:30, width: 30, marginRight: 10}} />
+                                <p className="options-text">Hide</p>
+                            </div>
+                            ) : null
+                        }
+                    </div>
+                    ) : null
+                }
             </div>
             <p className="profile-name pl-20 mb-10"> {name === "" ? "Company Name" : name}</p>
             <div className="flexRow flexBetween"> 
--- src/components/ProjectCard/ProjectCard.js
+++ src/components/ProjectCard/ProjectCard.js
@@ -1,6 +1,5 @@
 import '../../styles/HelperStyles.css';
 import './ProjectCard.css';
-import {BsThreeDotsVertical} from 'react-icons/bs';
 
 export default function ProjectCard({name, img, techstack}){
     return(
@@ -12,9 +11,6 @@ export default function ProjectCard({name, img, techstack}){
                 backgroundRepeat: 'no-repeat'
             }}
         >
-            <div className="flexRow flexCenter flexAlignCenter iconcontainer" style={{top: '-50%', left: '90%'}}>
-                <BsThreeDotsVertical className="portfolio-icon" size="20" />
-            </div>
             <div className="flexColumn flexCenter flexAlignCenter project-textbox">
                 <p className="project-name">{ name.length > 0 ? name : "Sample Webpage"}</p>
             </div>
