04c3aeed20b4b8129b0d80ddab5816ce9e350c01
Vikraantnegi <43957569+Vikraantnegi@users.noreply.github.com>
2020-12-30 17:16:02 +0530

    ADD: EmptyData

---
+++ src/assets/Datafiles/StudentB.js
@@ -0,0 +1,39 @@
+/* eslint-disable import/no-anonymous-default-export */
+import profile from '../images/StudentA/Ellipse 1.png';
+
+export default[
+    {
+        sr : 1,
+        title : '',
+        name : 'Tony Steve',
+        desc : '',
+        image : profile,
+        profile : [
+            {
+                sr : 1,
+                name : '',
+                id : '',
+                rank : '',
+                logo : '',
+            },
+        ],
+        achievements : [
+            {
+                sr : 1,
+                name : '',
+                logo : '',
+                id: '',
+                rank : '',
+                contest : ''
+            },
+        ],
+        projects : [
+            {
+                sr : 1,
+                name : '',
+                img : '',
+                techstack : '', 
+            },
+        ]
+    }
+]
