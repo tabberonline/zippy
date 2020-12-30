2e00ac1e5c5f4854f109bf9ab0258bed82e16660
Vikraantnegi <43957569+Vikraantnegi@users.noreply.github.com>
2020-12-30 15:16:40 +0530

    UPDATE : ContestCard

--- src/components/ContestCard/ContestCard.js
+++ src/components/ContestCard/ContestCard.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import '../../styles/HelperStyles.css';
 import './ContestCard.css';
 import {BsThreeDotsVertical} from 'react-icons/bs';
@@ -6,7 +6,6 @@ import deleted from '../../assets/images/Bin-Icon.png';
 import edited from '../../assets/images/Edit-Icon.png';
 import hidden from '../../assets/images/Hide-Icon.png';
 import hidecards from '../../assets/images/hiddeeen.png';
-import $ from 'jquery';
 
 export default function ContestCard({name, rank, id, logo, contest}){
 
@@ -16,60 +15,20 @@ export default function ContestCard({name, rank, id, logo, contest}){
     const [option2, setoption2] = useState(false);
     const [option3, setoption3] = useState(false);
     const [hidecard, sethide] = useState(false);
+    const [icon1, seticon1] = useState(true);
+    const [icon2, seticon2] = useState(true);
+    const [icon3, seticon3] = useState(true);
 
-    useEffect(() => {
-        $(".option-drawer").on("click", () => {
-            setdrawer(true);
-            setbullets(false);
-            // $(".options").mouseleave(() => {
-            //     setdrawer(false);
-            //     setbullets(true);
-            // })
-        })
-        $(".option-drawer").on("click", () => {
-            setdrawer(true);
-            setbullets(false);
-        })
-        $(".delete-icon").mouseenter(() => {
-            $(".delete-icon").hide();
-            setoption1(true);
-            
-            $(".delete-option").mouseleave(() => {
-                $(".delete-icon").show();
-                setoption1(false);
-            })        
-        })
-        $(".edit-icon").mouseenter(() => {
-            $(".edit-icon").hide();
-            setoption2(true);
-            
-            $(".edit-option").mouseleave(() => {
-                $(".edit-icon").show();
-                setoption2(false);
-            }) 
-        })
-        $(".hide-icon").mouseenter(() => {
-            $(".hide-icon").hide();
-            setoption3(true);
-            
-            $(".hide-option").mouseleave(() => {
-                $(".hide-icon").show();
-                setoption3(false);
-            }) 
-        })
-        $(".hide-option").on("click", () => {
-            sethide(true);
-            $(".hide-card-icon").on("click", () => {
-                sethide(false);
-            })
-        })
-    })
+    const OptionDrawer = () => {
+        setdrawer(true);
+        setbullets(false);
+    }
 
     return(
         <div className="card11 flexColumn achievement-card">
             {
                 hidecard ? (
-                    <div id="overlay" className="flexColumn flexCenter flexAlignCenter">
+                    <div id="overlay" onClick={() => sethide(false)} className="flexColumn flexCenter flexAlignCenter">
                         <img src={hidecards} alt="hidden" className="hide-card-icon" style={{height:30, width: 30, marginBottom: 10}} />
                         <p className="options-text" style={{color: 'white'}}>Hidden</p>
                     </div>
@@ -84,32 +43,32 @@ export default function ContestCard({name, rank, id, logo, contest}){
                     <img className="logo" src={logo} alt="logo" />
                 )}
                 { bullets ? (
-                    <div className="flexRow option-drawer flexCenter flexAlignCenter iconcontainer">
+                    <div onClick={() => OptionDrawer()} className="flexRow option-drawer flexCenter flexAlignCenter iconcontainer">
                         <BsThreeDotsVertical className="portfolio-icon" size="20" />                    
                     </div>
                     ) : null 
                 }
                 { drawer ? (
                     <div className="flexColumn flexStart options" style={{position: 'absolute', top: '-15%', right:'-2%'}}>
-                        <img src={deleted} alt="delete" className="delete-icon" style={{height:30, width: 30, marginBottom: 10, marginLeft: option2 ? 50 : null || option3 ? 50 : null}} />
+                        {icon1 ? (<img src={deleted} alt="delete" onMouseEnter={() => {setoption1(true); seticon1(false);}} className="delete-icon" style={{height:30, width: 30, marginBottom: 10, marginLeft: option2 ? 50 : null || option3 ? 50 : null}} />) : null }
                         { option1 ? (
-                            <div className="flexRow flexAlignCenter option delete-option" style={{ marginBottom: 10, position: 'relative', left: 40 }}>
+                            <div className="flexRow flexAlignCenter option delete-option" onMouseLeave={() => {setoption1(false); seticon1(true);}} style={{ marginBottom: 10, position: 'relative', left: 40 }}>
                                 <img src={deleted} alt="delete" style={{height:30, width: 30, marginRight: 10}} />
                                 <p className="options-text">Delete</p>
                             </div>                            
                             ) : null
                         }
-                        <img src={edited} alt="edit" className="edit-icon" style={{height:30, width: 30, marginBottom: 10, marginLeft: option1 ? 50 : null || option3 ? 50 : null}} />
+                        {icon2 ? (<img src={edited} alt="edit" onMouseEnter={() => {setoption2(true); seticon2(false);}} className="edit-icon" style={{height:30, width: 30, marginBottom: 10, marginLeft: option1 ? 50 : null || option3 ? 50 : null}} />) : null}
                         { option2 ? (
-                            <div className="flexRow flexAlignCenter option edit-option" style={{ marginBottom: 10, position: 'relative', left: 40 }}>
+                            <div className="flexRow flexAlignCenter option edit-option" onMouseLeave={() => {setoption2(false); seticon2(true);}} style={{ marginBottom: 10, position: 'relative', left: 40 }}>
                                 <img src={edited} alt="edit" style={{height:30, width: 30, marginRight: 10}} />
                                 <p className="options-text">Edit</p>
                             </div>
                             ) : null
                         }
-                        <img src={hidden} alt="hide" className="hide-icon" style={{height:30, width: 30, marginBottom: 10, marginLeft: option2 ? 50 : null || option1 ? 50 : null}} />
+                        {icon3 ? (<img src={hidden} alt="hide" onMouseEnter={() => {setoption3(true); seticon3(false);}} className="hide-icon" style={{height:30, width: 30, marginBottom: 10, marginLeft: option2 ? 50 : null || option1 ? 50 : null}} />) : null}
                         { option3 ? (
-                            <div className="flexRow flexAlignCenter option hide-option" style={{ marginBottom: 10, position: 'relative', left: 40 }}>
+                            <div className="flexRow flexAlignCenter option hide-option" onClick={() => {sethide(true);}} onMouseLeave={() => {setoption3(false); seticon3(true);}} style={{ marginBottom: 10, position: 'relative', left: 40 }}>
                                 <img src={hidden} alt="hide" style={{height:30, width: 30, marginRight: 10}} />
                                 <p className="options-text">Hide</p>
                             </div>
@@ -121,8 +80,8 @@ export default function ContestCard({name, rank, id, logo, contest}){
             </div>
             <p className="profile-name pl-20 mb-10"> {name === "" ? "Company Name" : name}</p>
             <div className="flexRow flexBetween"> 
-            <p className="profile-name ph-20 mb-10"><span className="profile-heading">ID:</span> {id === "" ?  "Id here" : id }</p>
-            <p className="profile-name ph-20 mb-10"><span className="profile-heading">Rank:</span> {rank === "" ?  "?" : rank  }</p>
+                <p className="profile-name ph-20 mb-10"><span className="profile-heading">ID:</span> {id === "" ?  "Id here" : id }</p>
+                <p className="profile-name ph-20 mb-10"><span className="profile-heading">Rank:</span> {rank === "" ?  "?" : rank  }</p>
             </div>
             <p className="profile-name ph-20 mb-10"><span className="profile-heading">Contest Name: </span>{contest === "" ? "Contest Name" : contest }</p>
         </div>
