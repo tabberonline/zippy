cb60d4342b2ba6f7a5dae6a13a6ca2dafe8077ca
Vikraantnegi <43957569+Vikraantnegi@users.noreply.github.com>
2021-01-06 07:51:46 +0530

    Update PortfolioModal.js

--- src/components/modals/PortfolioModal.js
+++ src/components/modals/PortfolioModal.js
@@ -2,46 +2,62 @@
 import React from 'react';
 import '../../styles/HelperStyles.css'
 import { Form, Modal } from 'react-bootstrap';
-import AdminService from '../../AdminServices/AdminService';
-import {AiOutlineCloseCircle, AiOutlinePlusCircle} from 'react-icons/ai';
+import {AiOutlineCloseCircle} from 'react-icons/ai';
 import { getItem, setItem } from '../../utility/localStorageControl';
 import Axios from 'axios';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 const API_ENDPOINT = 'https://whispering-eyrie-04211.herokuapp.com';
   
   export default function CodingProfileModal() {
     const [modalShow, setModalShow] = React.useState(false);
-    const [name, setname] = React.useState('');
-    const [desc, setdesc] = React.useState('');
-    const [title, settitle] = React.useState('');
-
-    const accessToken = getItem('access_token');
+    var name = '';
+    var title = '';
+    var desc = '';    
 
     const createPortfolio = async () => {
-        console.log('3rd call', getItem('access_token'));
-        // if (name.length > 0 & title.length > 0 & desc.length > 0 ){
-            const portfolioData = {
-                'title': getItem('title'),
-                'picture_url': getItem('image'),
-                'description': getItem('desc')
-            };
-            Axios.post(`${API_ENDPOINT}/portfolio/create`, portfolioData, {headers : {
-              'Authorization': `${accessToken}`,
-              'Content-Type': 'application/json',
-              'Accept': 'application/json',
-            }})
-              .then(resp => {
-                console.log('Response',resp);
-              })
-              .catch(err => {
-                console.log('Error',err.response);
-              })
-        // }
-    };
+      const accessToken = getItem('access_token');
+      if(accessToken === ""){
+        toast.error('Access Token not Retrieved!', {
+          position: "top-center",
+          autoClose: 2000,
+          hideProgressBar: true,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          });
+      } else{
+        if(title.length > 0 && desc.length > 0){
+          const portfolioData = {
+              'title': getItem('titlePortfolio'),
+              'picture_url': getItem('image'),
+              'description': getItem('descPortfolio')
+          };
+          console.log(portfolioData);
+          Axios.post(`${API_ENDPOINT}/portfolio/create`, portfolioData, {headers : {
+            'Authorization': `${accessToken}`,
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+          }})
+            .then(resp => {
+              console.log('Response',resp);
+            })
+            .catch(err => {
+              console.log('Error',err.response);
+            });
+        }
+      }
+    };  
 
     const Add = () => {
-      setItem('title', title);
-      setItem('desc', desc);
+      setItem('titlePortfolio', title);
+      setItem('descPortfolio', desc);
       createPortfolio();
+    }
+
+    const ModalOpen = () => {
+      setModalShow(true);
     }
 
     function MyVerticallyCenteredModal(props) {
@@ -61,21 +77,18 @@ const API_ENDPOINT = 'https://whispering-eyrie-04211.herokuapp.com';
               </button>
             </div>
             <Form>
-              <Form.Group controlId="formBasicEmail" className="mb-20">
-                <Form.Label>Name<span style={{color: 'red'}}>*</span></Form.Label>
-                <Form.Control type="text" defaultValue={name} onChange={(e) => setname(e.target.value)} placeholder="Eg. Aarav Bansal" />
-              </Form.Group>
-    
+              <Form.Group controlId="formBasic1" className="mb-20">
+                <Form.Label>Your Name<span style={{color: 'red'}}>*</span> </Form.Label>
+                <Form.Control type="text" defaultValue={name} onChange={(e) => name = (e.target.value)} placeholder="Eg. Aarav Bansal" />
+              </Form.Group>    
               <Form.Group controlId="formBasic1" className="mb-20">
                 <Form.Label>Your Portfolio Title<span style={{color: 'red'}}>*</span> </Form.Label>
-                <Form.Control type="text" defaultValue={title} onChange={(e) => settitle(e.target.value)} placeholder="Eg. Web Developer" />
-              </Form.Group>
-    
+                <Form.Control type="text" defaultValue={title} onChange={(e) => title = (e.target.value)} placeholder="Eg. Web Developer" />
+              </Form.Group>    
               <Form.Group controlId="formBasic2">
                 <Form.Label>Your Description<span style={{color: 'red'}}>*</span></Form.Label>
-                <Form.Control type="text" defaultValue={desc} onChange={(e) => setdesc(e.target.value)} placeholder="Enter your College and profile description here" />
-              </Form.Group>
-    
+                <Form.Control type="text" defaultValue={desc} onChange={(e) => desc = (e.target.value)} placeholder="Enter your College and profile description here" />
+              </Form.Group>    
             </Form>
     
             <div className="share" style={{justifyContent: 'center'}}>
@@ -89,11 +102,22 @@ const API_ENDPOINT = 'https://whispering-eyrie-04211.herokuapp.com';
   
     return (
       <>
-        <button onClick={() => setModalShow(true)} className="edit-your-portfolio">Edit your Portfolio</button>
+        <button onClick={() => ModalOpen()} className="edit-your-portfolio">Edit your Portfolio</button>
         <MyVerticallyCenteredModal
           show={modalShow}
           onHide={() => setModalShow(false)}
         />
+        <ToastContainer
+            position="top-center"
+            autoClose={3000}
+            hideProgressBar={true}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+          />
       </>
     );
   }
