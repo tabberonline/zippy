c3c8878108fe70b9ef6a31e6d9b93aa32f00e0dd
Vikraantnegi <43957569+Vikraantnegi@users.noreply.github.com>
2021-01-06 18:12:28 +0530

    Create ContestCardDisplay.js

---
+++ src/components/ContestCard/ContestCardDisplay.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import '../../styles/HelperStyles.css';
+import './ContestCard.css';
+import {BsThreeDotsVertical} from 'react-icons/bs';
+import deleted from '../../assets/images/Bin-Icon.png';
+import edited from '../../assets/images/Edit-Icon.png';
+import hidden from '../../assets/images/Hide-Icon.png';
+import hidecards from '../../assets/images/hiddeeen.png';
+
+export default function ContestCard({name, rank, id, logo, contest}){
+    return(
+        <div className="card11 flexColumn achievement-card">
+            <div className="flexRow flexCenter" style={{position: 'relative'}}>
+                {logo === "" ? (
+                    <div className="flexRow flexCenter flexAlignCenter empty-logo">
+                        <p className="profile-name" style={{fontWeight: 700, color: 'white'}}>LOGO</p>
+                    </div>
+                ) : (
+                    <img className="logo" src={logo} alt="logo" />
+                )}
+            </div>
+            <p className="profile-name pl-20 mb-10"> {name === "" ? "Company Name" : name}</p>
+            <div className="flexRow flexBetween"> 
+                <p className="profile-name ph-20 mb-10"><span className="profile-heading">ID:</span> {id === "" ?  "Id here" : id }</p>
+                <p className="profile-name ph-20 mb-10"><span className="profile-heading">Rank:</span> {rank === "" ?  "?" : rank  }</p>
+            </div>
+            <p className="profile-name ph-20 mb-10"><span className="profile-heading">Contest Name: </span>{contest === "" ? "Contest Name" : contest }</p>
+        </div>
+    );
+}
