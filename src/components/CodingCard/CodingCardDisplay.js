97ee12b2e8933293f9fac3b1e50ea2f32ce8fc96
Vikraantnegi <43957569+Vikraantnegi@users.noreply.github.com>
2021-01-06 18:12:20 +0530

    Create CodingCardDisplay.js

---
+++ src/components/CodingCard/CodingCardDisplay.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import '../../styles/HelperStyles.css';
+import './CodingCard.css';
+import {BsThreeDotsVertical} from 'react-icons/bs';
+
+export default function CodingCardDisplay({name, rank, id, logo}){
+    return(
+            <div className="card11 flexColumn profile-card">
+                
+                <div className="flexRow flexCenter" style={{position: 'relative'}}>
+                    {logo === "" ? (
+                        <div className="flexRow flexCenter flexAlignCenter empty-logo">
+                            <p className="profile-name" style={{fontWeight: 700, color: 'white'}}>LOGO</p>
+                        </div>
+                    ) : (
+                        <img className="logo" src={logo} alt="logo" />
+                    )}
+                </div>
+                <p className="profile-name pl-20 mb-10"> {name === "" ? "Company Name" : name}</p>
+                <p className="profile-name ph-20 mb-10"><span className="profile-heading">ID:</span> {id === "" ?  "Id here" : id }</p>
+                <p className="profile-name ph-20 mb-10"><span className="profile-heading">Rank:</span> {rank === "" ?  "?" : rank  }</p>
+            </div>
+    );
+}
