a8e55d3cae7f03d8a0a07c3a57bfdd46edaac024
Vikraantnegi <43957569+Vikraantnegi@users.noreply.github.com>
2021-01-06 19:55:10 +0530

    ADD: UpdatePortfolio API

--- src/screens/PortfolioScreen/PortfolioScreen.js
+++ src/screens/PortfolioScreen/PortfolioScreen.js
@@ -11,15 +11,17 @@ import ContestProfileModal from '../../components/modals/ContestProfileModal';
 import ProjectModal from '../../components/modals/ProjectModal';
 import ShareModal from '../../components/modals/ShareModal';
 import {AiOutlineEdit} from 'react-icons/ai';
-import {BsThreeDotsVertical} from 'react-icons/bs';
 import $ from 'jquery';
 import Header1 from '../../components/Header/Header1';
+import { getItem, setItem } from '../../utility/localStorageControl';
+import AdminService from '../../AdminServices/AdminService';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function PortfolioScreen() {
-  const [name, setname] = useState('');
-  const [image, setimage] = useState('');
-  const [title, settitle] = useState('');
-  const [desc, setdesc] = useState('');
+  const [name, setname] = useState(getItem('name'));
+  const [title, settitle] = useState(getItem('titlePortfolio'));
+  const [desc, setdesc] = useState(getItem('descPortfolio'));
   const Edit1 = () => {
     console.log('edittable1')
     $(".title").prop("readonly", false);
@@ -28,25 +30,90 @@ function PortfolioScreen() {
     console.log('edittable2')
     $(".desc").prop("readonly", false);
   } 
+  const updateTitle = (event) => {
+    settitle(event.target.value);
+    setItem('titlePortfolio', title);
+    UpdatePortfolio();
+  }
+  const updateDesc = (event) => {
+    setdesc(event.target.value);
+    setItem('descPortfolio', desc);
+    UpdatePortfolio();
+  }
+  const UpdatePortfolio = async () =>{
+    if(desc.length > 0 && title.length > 0){
+      const UpdatePortfolioData = {
+        'title': getItem('titlePortfolio'),
+        'description': getItem('descPortfolio'),
+      }
+      console.log(UpdatePortfolioData);
+      AdminService.updatePortfolio(UpdatePortfolioData)
+        .then(resp => {
+          console.log('Response', resp);
+          toast.success('Details Updated!', {
+            position: "top-center",
+            autoClose: 2000,
+            hideProgressBar: true,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+          });
+        })
+        .catch(err => {
+          console.log('Error', err);
+          toast.error('Error, Please retry!', {
+            position: "top-center",
+            autoClose: 2000,
+            hideProgressBar: true,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+          });
+        });
+    } else {
+      toast.error('Error, Fields cannot be empty!', {
+        position: "top-center",
+        autoClose: 2000,
+        hideProgressBar: true,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+    }
+  }
   return (
-    <div className="#portfolio-screen">
+    <div className="#portfolio-screen">      
+      <ToastContainer
+        position="top-center"
+        autoClose={3000}
+        hideProgressBar={true}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
       <Header1 />
       <ShareModal />
       <div className="mw1100">
         <div className="p-40 flexColumn portfolio-section">
           {StudentA.map(student => (
-            <div className="flexColumn" key={student.sr}>
+            <div className="flexColumn">
               <div className="flexRow flexCenter flexAlignCenter">
-                <input type="text" autoComplete="off" autoCorrect="off" autoCapitalize="off" spellCheck="false" className="title" defaultValue={student.title} onChange={(text) => settitle(text)} placeholder="Portfolio Title" readOnly />
+                <input type="text" autoComplete="off" autoCorrect="off" autoCapitalize="off" spellCheck="false" className="title" defaultValue={title} onBlur={(event) => updateTitle(event)} placeholder="Portfolio Title" readOnly />
                 <div className="flexRow flexCenter flexAlignCenter iconcontainer1">
                   <AiOutlineEdit className="portfolio-icon" size="20" onClick={()=>Edit1()} />
                 </div>
               </div>
               <hr style={{color : '#717070', width: '80%', margin: 'auto', marginTop: 10}} />
               <div className="flexColumn" style={{margin: 40, marginLeft: 0, marginBottom: 0}}>
-                <p className="name mb-20 pl-20">Hello! I am <strong>{student.name}</strong></p>
+                <p className="name mb-20 pl-20">Hello! I am <strong>{name}</strong></p>
                 <div className="flexRow">
-                  <textarea autocomplete="off" autoCorrect="off" autoCapitalize="off" spellCheck="false" rows="5" className="desc" defaultValue={student.desc} onChange={(text) => setdesc(text)} readOnly placeholder="Enter your College and profile description here" />
+                  <textarea autocomplete="off" autoCorrect="off" autoCapitalize="off" spellCheck="false" rows="5" className="desc" defaultValue={desc} onBlur={(event) => updateDesc(event)} readOnly placeholder="Enter your College and profile description here" />
                   <div className="flexRow flexCenter flexAlignCenter iconcontainer1" style={{left: -15, top: -15}}>
                     <AiOutlineEdit className="portfolio-icon" size="20" onClick={()=>Edit2()} />
                   </div>
