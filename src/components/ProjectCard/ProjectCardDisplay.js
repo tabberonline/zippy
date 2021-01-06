eebeb2ad66c6ee2aa93885fcd6ad4474cdc55eee
Vikraantnegi <43957569+Vikraantnegi@users.noreply.github.com>
2021-01-06 18:12:24 +0530

    Create ProjectCardDisplay.js

---
+++ src/components/ProjectCard/ProjectCardDisplay.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import '../../styles/HelperStyles.css';
+import './ProjectCard.css';
+import deleted from '../../assets/images/Bin-Icon.png';
+import edited from '../../assets/images/Edit-Icon.png';
+
+export default function ProjectCard({name, img, techstack}){
+    const [namecard, setcard] = useState(true);
+    return(
+        <div className="flexColumn project-card flexEnd" 
+            style={{  
+                backgroundImage: img === "" ? null : `url("${img}")`,
+                backgroundColor: 'rgba(219,219,219,1)',
+                backgroundSize: 'cover',
+                backgroundRepeat: 'no-repeat'
+            }}
+        >
+            {
+                namecard ? (
+                    <div className="flexColumn flexCenter flexAlignCenter project-textbox">
+                        <p className="project-name">{ name.length > 0 ? name : "Sample Webpage"}</p>
+                    </div>
+                ) : null
+            }
+        </div>
+    );
+}
