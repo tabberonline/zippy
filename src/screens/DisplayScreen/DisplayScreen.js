803e7058fa77a7fa6a511b12daebb674e738d327
Vikraantnegi <43957569+Vikraantnegi@users.noreply.github.com>
2021-01-06 18:12:32 +0530

    Update DisplayScreen.js

--- src/screens/DisplayScreen/DisplayScreen.js
+++ src/screens/DisplayScreen/DisplayScreen.js
@@ -4,9 +4,9 @@ import './DisplayScreen.css';
 import Header from '../../components/Header/Header';
 import Footer from '../../components/Footer/Footer';
 import StudentA from '../../assets/Datafiles/StudentA';
-import CodingCard from '../../components/CodingCard/CodingCard';
-import ContestCard from '../../components/ContestCard/ContestCard';
-import ProjectCard from '../../components/ProjectCard/ProjectCard';
+import CodingCardDisplay from '../../components/CodingCard/CodingCardDisplay';
+import ContestCardDisplay from '../../components/ContestCard/ContestCardDisplay';
+import ProjectCardDisplay from '../../components/ProjectCard/ProjectCardDisplay';
 
 function DisplayScreen() {
   return (
@@ -18,18 +18,15 @@ function DisplayScreen() {
             <div className="flexColumn" key={student.sr}>
               <p className="title">{student.title}</p>
               <hr style={{color : '#717070', width: '80%', margin: 'auto', marginTop: 10}} />
-              <div className="flexAlignCenter flexRow mv-40">
-                <img src={student.image} className="profile-pic" alt="profile" />
-                <div className="flexColumn" style={{marginLeft: 40, marginTop: 40}}>
-                  <p className="name mb-20 pl-20">Hello! I am <strong>{student.name}</strong></p>
-                  <p className="desc">{student.desc}</p>
-                </div>
+              <div className="flexColumn" style={{margin: 40, marginLeft: -20, marginBottom: 0}}>
+                <p className="name mb-20 pl-20">Hello! I am <strong>{student.name}</strong></p>
+                <p className="desc">{student.desc}</p>
               </div>
               <div className="coding-profile mv-20">
                 <p className="card-heading mb-20">Coding Profile</p>
                 <div className="flexRow flexWrap">
                   {student.profile.map(profile => (
-                    <CodingCard key={profile.sr} name={profile.name} id={profile.id} rank={profile.rank} logo={profile.logo} />
+                    <CodingCardDisplay key={profile.sr} name={profile.name} id={profile.id} rank={profile.rank} logo={profile.logo} />
                   ))}
                 </div>
               </div>
@@ -37,7 +34,7 @@ function DisplayScreen() {
                 <p className="card-heading mb-20">Contests Won</p>
                 <div className="flexRow flexWrap">
                   {student.achievements.map(achievement => (
-                    <ContestCard key={achievement.sr} name={achievement.name} id={achievement.id} rank={achievement.rank} logo={achievement.logo} contest={achievement.contest} />
+                    <ContestCardDisplay key={achievement.sr} name={achievement.name} id={achievement.id} rank={achievement.rank} logo={achievement.logo} contest={achievement.contest} />
                   ))}
                 </div>
               </div>
@@ -45,7 +42,7 @@ function DisplayScreen() {
                 <p className="card-heading mb-20">Personal Projects</p>
                 <div className="flexRow flexWrap">
                   {student.projects.map(project => (
-                    <ProjectCard key={project.sr} name={project.name} img={project.img} techstack={project.techstack} />
+                    <ProjectCardDisplay key={project.sr} name={project.name} img={project.img} techstack={project.techstack} />
                   ))}
                 </div>
               </div>
