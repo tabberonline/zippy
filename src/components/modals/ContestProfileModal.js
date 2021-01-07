76b5b3b6151532b8e10c632aedbd325cc6cd2a7e
Vikraantnegi <43957569+Vikraantnegi@users.noreply.github.com>
2021-01-07 20:12:52 +0530

    Update ContestProfileModal.js

--- src/components/modals/ContestProfileModal.js
+++ src/components/modals/ContestProfileModal.js
@@ -4,56 +4,60 @@ import '../../styles/HelperStyles.css'
 import { Modal, Form } from 'react-bootstrap';
 import {AiOutlineCloseCircle, AiOutlinePlusCircle} from 'react-icons/ai';
 
-function MyVerticallyCenteredModal(props) {
-  return (
-    <Modal
-      {...props}
-      size="lg"
-      aria-labelledby="contained-modal-title-vcenter"
-      centered
-    >
-      <div className="flexColumn">
-        <div className="flexRow flexBetween flexAlignCenter mb-40">
-          <div style={{width: 20}}></div>
-          <h2 className="modal-head">Add Contests won</h2>
-          <button onClick={props.onHide}>
-            <AiOutlineCloseCircle style={{fontSize: 40, color: 'black'}} />
-          </button>
-        </div>
-        <Form>
-          <Form.Group controlId="formBasicEmail" className="mb-20">
-            <Form.Label>Website Name</Form.Label>
-            <Form.Control type="website" placeholder="Eg. https://www.hackerrank.com" />
-          </Form.Group>
-
-          <Form.Group controlId="formBasicPassword" className="mb-20">
-            <Form.Label>Your Profile ID </Form.Label>
-            <Form.Control type="text" placeholder="Eg. abc_234" />
-          </Form.Group>
-
-          <Form.Group controlId="formBasicPassword" className="mb-20">
-            <Form.Label>Contest Name</Form.Label>
-            <Form.Control type="text" placeholder="Eg. 2020 Hackathon" />
-          </Form.Group>
-
-          <Form.Group controlId="formBasicPassword">
-            <Form.Label>Your Rank</Form.Label>
-            <Form.Control type="text" placeholder="Eg. 25" />
-          </Form.Group>
-
-        </Form>
-
-        <div className="share" style={{justifyContent: 'center'}}>
-          <a onClick={props.onHide} className="flexAlignCenter modal-button">Add to Profile</a>
-        </div>
-
-      </div>
-    </Modal>
-  );
-}
-
 export default function ContestProfileModal() {
   const [modalShow, setModalShow] = React.useState(false);
+  var portal = '';
+  var contest = '';
+  var rank = '';
+  var username = '';
+
+  function MyVerticallyCenteredModal(props) {
+    return (
+      <Modal
+        {...props}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <div className="flexColumn">
+          <div className="flexRow flexBetween flexAlignCenter mb-40">
+            <div style={{width: 20}}></div>
+            <h2 className="modal-head">Add Contests won</h2>
+            <button onClick={props.onHide}>
+              <AiOutlineCloseCircle style={{fontSize: 40, color: 'black'}} />
+            </button>
+          </div>
+          <Form>
+            <Form.Group controlId="formBasicEmail" className="mb-20">
+              <Form.Label>Website Name</Form.Label>
+              <Form.Control type="text" placeholder="Eg. GeeksforGeeks, CodeChef" defaultValue={portal} onChange={(e) => portal = (e.target.value)} />
+            </Form.Group>
+  
+            <Form.Group controlId="formBasicPassword" className="mb-20">
+              <Form.Label>Your Profile Username </Form.Label>
+              <Form.Control type="text" placeholder="Eg. abc_234" defaultValue={username} onChange={(e) => username = (e.target.value)} />
+            </Form.Group>
+  
+            <Form.Group controlId="formBasicPassword" className="mb-20">
+              <Form.Label>Contest Name</Form.Label>
+              <Form.Control type="text" placeholder="Eg. 2020 Hackathon" defaultValue={contest} onChange={(e) => contest = (e.target.value)} />
+            </Form.Group>
+  
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label>Your Rank</Form.Label>
+              <Form.Control type="text" placeholder="Eg. 2514" defaultValue={rank} onChange={(e) => rank = (e.target.value)} />
+            </Form.Group>
+  
+          </Form>
+  
+          <div className="share" style={{justifyContent: 'center'}}>
+            <a onClick={props.onHide} className="flexAlignCenter modal-button">Add to Profile</a>
+          </div>
+  
+        </div>
+      </Modal>
+    );
+  }
 
   return (
     <>
