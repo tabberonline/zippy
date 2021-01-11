af41fa795d69b9f342d4d59bcd05b74b4c7df57d
Vikraantnegi <43957569+Vikraantnegi@users.noreply.github.com>
2021-01-12 03:05:16 +0530

    Create reducer.js

---
+++ src/utility/reducer.js
@@ -0,0 +1,47 @@
+export const initialState = {
+    user_id: null,
+    token: null,
+    login: false,
+    rankWidgets: [],
+    contestWidgets: [],
+    projects: [],
+}
+
+const reducer = (state, action) => {
+    switch(action.type){
+        case 'SET_USER':
+            return {
+                ...state,
+                user_id: action.user
+            }
+        case 'SET_TOKEN':
+            return {
+                ...state,
+                token: action.token
+            }
+        case 'SET_LOGIN':
+            return {
+                ...state,
+                login: action.token
+            }
+        case 'SET_RANK_WIDGETS':
+            return {
+                ...state,
+                rankWidgets: action.token
+            }
+        case 'SET_CONTEST_WIDGETS':
+            return {
+                ...state,
+                contestWidgets: action.token
+            }
+        case 'SET_PROJECTS':
+            return {
+                ...state,
+                projects: action.token
+            }
+        default:
+            return state;
+    }
+}
+
+export default reducer;
