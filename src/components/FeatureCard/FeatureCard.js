5fc6701b6001447c05d7668b765385225543d010
Vikraantnegi <43957569+Vikraantnegi@users.noreply.github.com>
2021-01-20 02:20:04 +0530

    Update FeatureCard.js

--- src/components/FeatureCard/FeatureCard.js
+++ src/components/FeatureCard/FeatureCard.js
@@ -5,7 +5,7 @@ import '../../styles/HelperStyles.css';
 function FeatureCard({name, img, desc}){
     return (
         <div className='grow1 feature-card'>
-            <div className="flexColumn flexAlignCenter">
+            <div className="flexColumn flexAlignCenter flexCenter">
                 <div style={{height: 120, width: 120, borderRadius: 60, backgroundImage: `url(${img})`}}></div>
                 <p className="feature-text">{name}</p>
                 <p className="feature-innertext">{desc}</p>
