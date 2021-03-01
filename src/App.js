cec8922c2073d30d0f25eb461f5eb00cc373b724
Vikraantnegi <43957569+Vikraantnegi@users.noreply.github.com>
2021-03-02 04:14:57 +0530

    HOST

--- src/App.js
+++ src/App.js
@@ -1,21 +1,8 @@
 import React, {useEffect} from 'react';
 import './App.css';
 import HomeScreen from './screens/HomeScreen/HomeScreen';
-import {setItem, getItem} from './utility/localStorageControl';
 
 function App() {  
-  useEffect(() => {
-    setItem('login', false);
-    setItem('access_token', '');
-    setItem('name', '');
-    setItem('image', '');
-    setItem('portfolio', false);
-    setItem('projects', []);
-    setItem('rank', []);
-    setItem('contest', []);
-    setItem('resumeLink', '');
-    
-  }, []);
   return (
     <div className="App">
       <HomeScreen />
