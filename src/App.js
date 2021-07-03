c8bd938d6ddc03478d9e3b247ad9afdb488ffa12
vikraantnegi <43957569+Vikraantnegi@users.noreply.github.com>
2021-07-03 11:11:59 +0530

    fix App.js

--- src/App.js
+++ src/App.js
@@ -8,32 +8,29 @@ import DisplayScreen from './screens/DisplayScreen/DisplayScreen';
 import PolicyScreen from './screens/PolicyScreen/PolicyScreen';
 import TermsScreen from './screens/TermsScreen/TermsScreen';
 import Error404 from './screens/Error404';
-import { userToken } from './features/userSlice/userSlice';
-import {useSelector, useDispatch} from 'react-redux';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Redirect
 } from "react-router-dom";
+import { getItem } from './utility/localStorageControl';
 
 function App() {
-  const dispatch = useDispatch();
-  const accessToken = useSelector(userToken);
   return (    
     <div className="App">
       <Router>
-      <Switch>
+        <Switch>
             <Route exact path="/" component={App} />
             <Route exact path="/home" component={HomeScreen} /> 
-            <Route exact path="/portfolio">{accessToken ? <PortfolioScreen /> : <Redirect to="/home" />}</Route> 
+            <Route exact path="/portfolio">{getItem('accessToken') ? <PortfolioScreen /> : <Redirect to="/home" />}</Route> 
             <Route exact path="/d" component={DisplayScreen} /> 
             <Route exact path="/about" component={AboutScreen} /> 
             <Route exact path="/contact" component={ContactScreen} />
             <Route exact path="/policy" component={PolicyScreen} />
             <Route exact path="/terms" component={TermsScreen} />
             <Route component={Error404} />
-        </Switch>
+        </Switch>            
       </Router>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -2,34 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
-import HomeScreen from './screens/HomeScreen/HomeScreen';
-import PortfolioScreen from './screens/PortfolioScreen/PortfolioScreen';
-import AboutScreen from './screens/AboutScreen/AboutScreen';
-import ContactScreen from './screens/ContactScreen/ContactScreen';
-import DisplayScreen from './screens/DisplayScreen/DisplayScreen';
-import PolicyScreen from './screens/PolicyScreen/PolicyScreen';
-import TermsScreen from './screens/TermsScreen/TermsScreen';
 import { UserProvider } from './utility/userContext';
-import { getItem } from './utility/localStorageControl';
-import Error404 from './screens/Error404';
 
 ReactDOM.render(
   <React.StrictMode>
     <UserProvider>
-      <Router>
-        <Switch>
-            <Route exact path="/" component={App} />
-            <Route exact path="/home" component={HomeScreen} /> 
-            <Route exact path="/portfolio">{getItem('accessToken') ? <PortfolioScreen /> : <Redirect to="/home" />}</Route> 
-            <Route exact path="/d" component={DisplayScreen} /> 
-            <Route exact path="/about" component={AboutScreen} /> 
-            <Route exact path="/contact" component={ContactScreen} />
-            <Route exact path="/policy" component={PolicyScreen} />
-            <Route exact path="/terms" component={TermsScreen} />
-            <Route component={Error404} />
-        </Switch>            
-      </Router>
+        <App />
     </UserProvider>
   </React.StrictMode>,
   document.getElementById('root')
