c2f854e474d3e021f50248bf2f293ec1a140a279
vikraantnegi <43957569+Vikraantnegi@users.noreply.github.com>
2021-07-10 15:01:15 +0530

    UserToken fetch fix

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -23,6 +23,7 @@ const userSlice = createSlice({
         logUser: (state, action) => {
             state.login = true;
             state.accessToken = action.payload.access_token;
+            return state;
         },
         logOutUser: (state, action) => {
             state.login = false;
--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -65,7 +65,7 @@ function HomeScreen() {
         <div id="intro-section">
           <div className="mw1100 mobile-column flexRow flexAround flexAlignCenter">
             <div className="embed-responsive embed-responsive-16by9">
-              <iframe title="Tabber Advertisement Video" frameborder="0" className="embed-responsive-item" src="https://www.youtube.com/embed/_2S0x-YV3XQ?rel=0" allowFullScreen />
+              <iframe title="Tabber Advertisement Video" frameBorder="0" className="embed-responsive-item" src="https://www.youtube.com/embed/_2S0x-YV3XQ?rel=0" allowFullScreen />
             </div>
             <div className="mw600 mobile-center flexColumn">
                 <h1 className="intro-text">
