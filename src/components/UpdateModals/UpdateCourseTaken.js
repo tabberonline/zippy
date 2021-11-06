bfc4d5bba67ea46060936824269d302cbb63b8c7
vikraantnegi <43957569+Vikraantnegi@users.noreply.github.com>
2021-11-06 23:58:25 +0530

    Edit Course Modal

---
+++ src/components/UpdateModals/UpdateCourseTaken.js
@@ -0,0 +1,140 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from "react";
+import "../../styles/HelperStyles.css";
+import { Form, Modal } from "react-bootstrap";
+import { AiOutlineCloseCircle } from "react-icons/ai";
+import {
+  SuccessToast,
+  ErrorToast,
+} from "../../utility/localStorageControl";
+import AdminService from "../../AdminServices/AdminService";
+import edited from "../../assets/images/Edit-Icon.png";
+import { useDispatch } from "react-redux";
+import { setCourses } from "../../features/user/userSlice";
+
+export default function UpdateCourseTaken({
+  name,
+  link,
+  issuer
+}) {
+  const dispatch = useDispatch();
+  const [modalShow, setModalShow] = React.useState(false);
+
+  let CourseName = name;
+  let InstituteName = issuer;
+  let CourseLink = link;
+
+  const updateCourseWidget = async () => {
+    if (portal && username && rank) {
+      let CourseData = {
+        "course_name": CourseName,
+        "issuer": InstituteName,
+        "certificate_link": CourseLink
+      };
+      AdminService.updateCourseWidget(CourseData)
+        .then(() => {
+          SuccessToast("Course Updated!");
+          AdminService.getUserData()
+            .then((resp) => {
+              dispatch(setCourses(resp.data));
+              close();
+              setModalShow(false);
+            })
+            .catch((err) => ErrorToast("Some Error Occured."));
+          close();
+        })
+        .catch((error) => {
+          ErrorToast("Error, Enter correct details!");
+          close();
+        });
+    } else {
+      ErrorToast("Error, Fields cannot be empty!");
+      close();
+    }
+  };
+
+  const UpdateCard = () => {
+    open();
+    updateCourseWidget();
+  };
+
+  function MyVerticallyCenteredModal(props) {
+    return (
+      <Modal
+        {...props}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+        animation={false}
+      >
+        <div className="flexColumn">
+          <div className="flexRow flexBetween flexAlignCenter mb-40">
+            <div style={{ width: 20 }}></div>
+            <h2 className="modal-head">Update Course Taken</h2>
+            <button onClick={props.onHide}>
+              <AiOutlineCloseCircle style={{ fontSize: 40, color: "black" }} />
+            </button>
+          </div>
+          <Form>
+            <Form.Group controlId="formBasicEmail" className="mb-20">
+              <Form.Label>Course Name*</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Eg.  Data Structures & Algorithm"
+                defaultValue={CourseName}
+                onChange={(e) => (CourseName = e.target.value)}
+              />
+            </Form.Group>
+
+            <Form.Group controlId="formBasicEmail2" className="mb-20">
+              <Form.Label>
+                Institute Name*
+              </Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Eg.  Coursera"
+                defaultValue={InstituteName}
+                onChange={(e) => (InstituteName = e.target.value)}
+              />
+            </Form.Group>
+
+            <Form.Group controlId="formBasicEmail3" className="mb-20">
+              <Form.Label>Course Certification Link</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Eg.  https://www.google.com"
+                defaultValue={CourseLink}
+                onChange={(e) => (CourseLink = e.target.value)}
+              />
+            </Form.Group>
+          </Form>
+
+          <div className="share" style={{ justifyContent: "center" }}>
+            <a
+              onClick={() => {UpdateCard(); setModalShow(false);}}
+              className="flexAlignCenter modal-button"
+            >
+              Update Course
+            </a>
+          </div>
+        </div>
+      </Modal>
+    );
+  }
+
+  return (
+    <>
+      <img
+        src={edited}
+        alt="edit"
+        onClick={() => setModalShow(true)}
+        style={{ height: 30, width: 30, marginRight: 10 }}
+      />
+
+      <MyVerticallyCenteredModal
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+      />
+    </>
+  );
+}
