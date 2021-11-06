480a1cf91e86cdb6a270456ba363e28f57df86fa
vikraantnegi <43957569+Vikraantnegi@users.noreply.github.com>
2021-11-06 15:51:41 +0530

    Course in Display Section

---
+++ src/components/CourseCard/CourseCardDisplay.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const CourseCardDisplay = ({name, issuer, link}) => {
+    return (
+        <div className="course-card flexRow flexAlignCenter">
+            {/* <img 
+                src="https://play-lh.googleusercontent.com/qq5__wITsoCx2kUK8TqVW2-8UDRuxET9kCzPzAPHad8umXiHRF2N0tZKuLezd0tiBQg" 
+                className="course__logo"  
+            /> */}
+            <div className="flexColumn">
+                <a href={link} className="course__name" >{name}</a>
+                <span className="course__issuer">{issuer}</span>
+            </div>
+        </div>
+    )
+}
