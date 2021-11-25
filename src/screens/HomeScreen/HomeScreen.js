b71ac1ef8aabe8ddd51dce227a2c704434a72273
vikrant <vikrant.negi74@gmail.com>
2021-11-25 21:53:35 +0530

    Separate Component

--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -105,12 +105,7 @@ function HomeScreen() {
               {console.log(TrendingProf)}
               {/* {TrendingProf.map(profile => {
                 return (
-                  <div className="trending__profileCard flexColumn flexAlignCenter">
-                    <img src={profile.picture_url} className="trending__profileImage" alt="UserImage" />
-                    <span className="trending__profileName">{profile.name}</span>
-                    <span className="trending__profileCollege">{profile.portfolio.college.split(',')[0]}</span>                 
-                    <span className="trending__profileViews">{profile.portfolio.views}{" "}Views</span>
-                  </div>
+                  
                 )
               })} */}
             </div>
