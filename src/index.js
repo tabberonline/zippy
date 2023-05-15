7d8e9383a66f8d8621c09b2d22fef3aeee7e3d75
mandeepsidhu2 <mandeep.sidhu2@gmail.com>
2023-05-15 17:23:13 -0400

    added comments

--- src/index.js
+++ src/index.js
@@ -6,6 +6,25 @@ import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react'
 import {store, persistor} from './app/store';
 
+
+// React and ReactDOM are libraries that enable you to build user interfaces using React components and render them to the DOM.
+
+// The line import './index.css'; imports a CSS file (index.css) that contains styles specific to this application.
+
+// App is a component imported from the App.js file, which represents the main component of the application.
+
+// The Provider component is imported from the 'react-redux' library and is used to provide the Redux store to the React components.
+
+// The PersistGate component is imported from the 'redux-persist/integration/react' library and is used to persist and 
+//rehydrate the Redux store.
+
+// The store and persistor are imported from the './app/store' file. We'll explain the contents of this file separately.
+
+// The ReactDOM.render() function is called to render the application. It takes two arguments:
+
+// The JSX code within the parentheses, which represents the structure of the application.
+// The document.getElementById('root') expression, which identifies the DOM element where the application will be rendered.
+
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
